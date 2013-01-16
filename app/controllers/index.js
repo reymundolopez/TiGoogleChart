ac2dcb013b2b9c94443ab7d6270518de8938e2e8
Reymundo López <reymundolopez@gmail.com>
2013-01-16 16:21:51 -0600

    Se agrega el .html y el código necesario para hacer las llamadas

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,13 @@
-function doClick(e) {  
-    alert($.label.text);
-}
 
 $.index.open();
+
+setTimeout(function(e){
+	Ti.App.fireEvent("drawTheChart", { data : [
+		["Weeks", "Rey" , "Caro", "Pablo"],
+		["1" , 4, 6, 2],
+		["2" , 6, 4, 4],
+		["3" , 3, 3, 4],
+		["4" , 6, 8, 10],
+		]}
+	);
+}, 2000);
